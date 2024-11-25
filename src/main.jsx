a8c172a0036467507686647b4f161c57199d35d7
Debojyoti Tantra <tantradebojyoti@gmail.com>
2024-11-25 13:23:09 +0530

    upgraded

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,27 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import About from './components/About';
+import Contact from './components/Contact';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+const router = createBrowserRouter([
+   {
+      path: "/",
+      element: <App />
+   },
+   {
+      path: "/contact",
+      element: <Contact />
+   },
+   {
+      path: "/about",
+      element: <About />
+   }
+])
 
 createRoot(document.getElementById('root')).render(
-//  <StrictMode>
-    <App />
-//  </StrictMode>,
+ <>
+   <RouterProvider router={router} />
+ </>,
 )
